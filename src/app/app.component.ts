040d26764dbf49cb2640e296773b9ddcb641cd5f
Debasish Panda <pandadebasish57@gmail.com>
2021-06-09 22:45:29 +0530

    ce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'animated';
+  data=[
+    {
+      heading:"FREE",
+      price:"0$",
+      user:"Single User",
+      storage:"5GB",
+      subdomain:"Free Subdomain",
+      color1:"text-muted",
+      color2:"text-muted",
+      color3:"text-muted",
+      color4:"text-muted",
+      fa1:"fas fa-check",
+      fa2:"fas fa-times",
+      fa3:"fas fa-times",
+      fa4:"fas fa-times",
+      fa5:"fas fa-times"
+
+    },
+    {
+      heading:"PLUS",
+      price:"9$",
+      user:"5 Users",
+      storage:"50GB",
+      subdomain:"Free Subdomain",
+      color1:"",
+      color2:"",
+      color3:"",
+      color4:"text-muted",
+      fa1:"fas fa-check",
+      fa2:"fas fa-check",
+      fa3:"fas fa-check",
+      fa4:"fas fa-check",
+      fa5:"fas fa-times"
+
+    },
+    {
+      heading:"PRO",
+      price:"49$",
+      user:"Unlimited Users",
+      storage:"150GB",
+      subdomain:"Unlimited Free Subdomain",
+      color1:"",
+      color2:"",
+      color3:"",
+      color4:"",
+      fa1:"fas fa-check",
+      fa2:"fas fa-check",
+      fa3:"fas fa-check",
+      fa4:"fas fa-check",
+      fa5:"fas fa-check"
+
+    }
+  ]
 }
